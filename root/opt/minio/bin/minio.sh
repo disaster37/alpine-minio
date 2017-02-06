92621b6baf3faee16581f10187059067f9ed236b
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-06 14:05:08 +0100

    Fix init script

--- root/opt/minio/bin/minio.sh
+++ root/opt/minio/bin/minio.sh
@@ -1,6 +1,8 @@
 #!/sbin/openrc-run
 
 
+DAEMON="${SERVICE_HOME}/bin/minio"
+
 start() {
   if [ "${RC_CMD}" = "restart" ];
   then
@@ -9,14 +11,12 @@ start() {
 
   ebegin "Starting minio"
   source ${SERVICE_HOME}/conf/minio-server.cfg
-  start-stop-daemon --start --exec ${SERVICE_HOME}/bin/minio server ${MINIO_VOLUMES} \
-    --pidfile ${SERVICE_HOME}/minio.pid
+  start-stop-daemon --background --name minio --start --exec ${SERVICE_HOME}/bin/minio server ${MINIO_VOLUMES}
   eend $?
 }
 
 stop() {
   ebegin "Stopping minio"
-  start-stop-daemon --stop --exec ${SERVICE_HOME}/bin/minio \
-    --pidfile ${SERVICE_HOME}/minio.pid
+  start-stop-daemon --name minio --stop
   eend $?
 }
