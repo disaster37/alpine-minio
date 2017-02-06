6c8ad6b3b21b7664a6fad1930a2d6e18f9836938
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-06 14:27:06 +0100

    Fix init script

--- root/opt/minio/bin/minio.sh
+++ root/opt/minio/bin/minio.sh
@@ -1,22 +1,46 @@
-#!/sbin/openrc-run
+#!/bin/sh -e
+$DAEMON="${SERVICE_HOME}/bin/minio"
+$DAEMON_NAME=minio
+PATH="/sbin:/bin:/usr/sbin:/usr/bin"
+
+test -x $DAEMON || exit 0
 
 
-DAEMON="${SERVICE_HOME}/bin/minio"
 
-start() {
-  if [ "${RC_CMD}" = "restart" ];
-  then
-    stop
-  fi
-
-  ebegin "Starting minio"
-  source ${SERVICE_HOME}/conf/minio-server.cfg
-  start-stop-daemon --background --name minio --start --exec ${SERVICE_HOME}/bin/minio server ${MINIO_VOLUMES}
-  eend $?
+d_start () {
+        echo "Starting ${DAEMON_NAME}"
+        source ${SERVICE_HOME}/conf/minio-server.cfg
+	      start-stop-daemon --background --name ${DAEMON_NAME} --start --quiet --exec $DAEMON server ${MINIO_VOLUMES}
+        echo $?
 }
 
-stop() {
-  ebegin "Stopping minio"
-  start-stop-daemon --name minio --stop
-  eend $?
+d_stop () {
+        echo "Stopping ${DAEMON_NAME}"
+        start-stop-daemon --name ${DAEMON_NAME} --stop --retry 5 --quiet
+	      echo $?
 }
+
+case "$1" in
+
+        start|stop)
+                d_${1}
+                ;;
+
+        restart|reload|force-reload)
+                        d_stop
+                        d_start
+                ;;
+
+        force-stop)
+               d_stop
+                killall -q ${DAEMON_NAME} || true
+                sleep 2
+                killall -q -9 ${DAEMON_NAME} || true
+                ;;
+
+        *)
+                echo "Usage: minio.sh {start|stop|force-stop|restart|reload|force-reload}"
+                exit 1
+                ;;
+esac
+exit 0
