f5454a84f0945cf963d740a152a73d8686ee4fdb
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-06 12:01:48 +0100

    Fix cfg script

--- root/opt/minio/bin/minio-server.cfg.sh
+++ root/opt/minio/bin/minio-server.cfg.sh
@@ -3,7 +3,7 @@
 MINIO_DATA=${MINIO_DATA:-"${SERVIVE_HOME}/data"}
 
 
-cat << EOF > ${SERVICE_VOLUME}/confd/etc/templates/minio-server.cfg.tmpl
+cat << EOF > ${SERVICE_CONF}
 MINIO_DATA = ${MINIO_DATA}
 MINIO_VOLUMES=${MINIO_DATA}
 EOF
