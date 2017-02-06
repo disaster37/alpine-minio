5faa096b048a8b3cf66a50c9e34cbdcb573eab3a
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-06 15:57:05 +0100

    Fix init script

--- root/opt/minio/bin/minio.sh
+++ root/opt/minio/bin/minio.sh
@@ -10,7 +10,7 @@ test -x ${DAEMON} || exit 0
 d_start () {
         echo "Starting ${DAEMON_NAME}"
         source ${SERVICE_HOME}/conf/minio-server.cfg
-	      start-stop-daemon --background --name ${DAEMON_NAME} --start --quiet --stdout ${SERVICE_HOME}/logs/minio.log --exec ${DAEMON} server ${MINIO_VOLUMES}
+	      start-stop-daemon --background --name ${DAEMON_NAME} --start --quiet --stdout ${SERVICE_HOME}/logs/minio.log --stderr ${SERVICE_HOME}/logs/minio.log --exec ${DAEMON} server ${MINIO_VOLUMES}
         echo $?
 }
 
