36aafc1910d9a3de45f430f3188a96979239fe86
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-07 17:04:24 +0100

    Fix start script

--- root/opt/confd/bin/service-confd-app.sh
+++ root/opt/confd/bin/service-confd-app.sh
@@ -26,19 +26,19 @@ function checkNetwork {
 
 function serviceTemplate {
     log "[ Checking ${CONF_NAME} template... ]"
-    bash ${CONF_HOME}/bin/gen.conf.tmpl.sh
+    bash ${CONF_HOME}/bin/gen.conf-app.tmpl.sh
 }
 
 function serviceStart {
     checkNetwork
     serviceTemplate
     log "[ Starting ${CONF_NAME}... ]"
-    /usr/bin/nohup ${CONF_INTERVAL} &> ${CONF_HOME}/log/confd.log &
+    start-stop-daemon --background --name ${CONF_NAME} --start --quiet --stdout ${SERVICE_HOME}/logs/${CONF_NAME}.log --stderr ${SERVICE_HOME}/logs/${CONF_NAME}.log --pidfile ${SERVICE_HOME}/${CONF_NAME}.pid --exec ${CONF_BIN} -- ${CONF_OPTS}
 }
 
 function serviceStop {
     log "[ Stoping ${CONF_NAME}... ]"
-    /usr/bin/killall confd
+    start-stop-daemon --name ${CONF_NAME} --retry 5 --oknodo --stop --quiet --pidfile ${SERVICE_HOME}/${CONF_NAME}.pid --exec ${CONF_BIN}  > /dev/null 2>&1
 }
 
 function serviceRestart {
@@ -48,7 +48,7 @@ function serviceRestart {
     /opt/monit/bin/monit reload
 }
 
-CONF_NAME=confd
+CONF_NAME=confd-app
 CONF_HOME=${CONF_HOME:-"/opt/tools/confd"}
 CONF_LOG=${CONF_LOG:-"${CONF_HOME}/log/confd.log"}
 CONF_BIN=${CONF_BIN:-"${CONF_HOME}/bin/confd"}
@@ -57,7 +57,7 @@ CONF_PREFIX=${CONF_PREFIX:-"/2015-12-19"}
 CONF_INTERVAL=${CONF_INTERVAL:-60}
 CONF_PARAMS=${CONF_PARAMS:-"-confdir /opt/tools/confd/etc -backend ${CONF_BACKEND} -prefix ${CONF_PREFIX}"}
 CONF_ONETIME="${CONF_BIN} -onetime ${CONF_PARAMS}"
-CONF_INTERVAL="${CONF_BIN} -interval ${CONF_INTERVAL} ${CONF_PARAMS}"
+CONF_OPTS="-interval ${CONF_INTERVAL} ${CONF_PARAMS}"
 
 case "$1" in
         "start")
