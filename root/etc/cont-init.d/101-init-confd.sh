a4ac15966112e11b438a250a98e20f0f144ef64f
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-20 15:47:58 +0100

    fix confd

--- root/etc/cont-init.d/101-init-confd.sh
+++ root/etc/cont-init.d/101-init-confd.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/with-contenv sh
 
 if [ "${CONFD_PREFIX_URL}X" == "X" ]; then
   PREFIX=""
@@ -10,6 +10,11 @@ if [ "${CONFD_NODES}X" == "X" ]; then
   NODE=""
 else
   NODE="-node ${CONFD_NODES}"
+fi
+
+# Get setting from scheduler
+if [ -f "${SCHEDULER_VOLUME}/conf/scheduler.cfg" ]; then
+  source "${SCHEDULER_VOLUME}/conf/scheduler.cfg"
 fi
 
 exec ${CONFD_HOME}/bin/confd -confdir ${CONFD_HOME}/etc -sync-only -onetime -backend ${CONFD_BACKEND} ${PREFIX} ${NODE}
